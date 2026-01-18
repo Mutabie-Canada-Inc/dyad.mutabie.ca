0970787d2f6c42e9bb84a441f6cb1cc7d3e80819
[dyad] <securedbread@protonmail.com>
2026-01-18 01:04:11 -0800

    [dyad] Fixing Vercel build issues - wrote 3 file(s) + extra files edited outside of Dyad

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  swcMinify: true,
+  output: 'standalone',
+  images: {
+    unoptimized: true
+  },
+  experimental: {
+    appDir: true
+  }
+}
 
-export default nextConfig;
+module.exports = nextConfig
